7d14a06343929c3b7998b5bf77470f6243e62d3d
ujinthegrey <v4deus@gmail.com>
2024-02-26 13:33:00 +0400

    add error element to a router

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       { path: '/', element: <Home /> },
-      { path: '/menu', element: <Menu/>, loader: menuLoader },
+      { path: '/menu', element: <Menu/>, loader: menuLoader, errorElement: <Error /> },
       { path: '/cart', element: <Cart /> },
       { path: '/order/new', element: <CreateOrder /> },
       { path: '/order/:orderId', element: <Order /> },
