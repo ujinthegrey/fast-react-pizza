fe7a6233254b0101e10c628b2c4c12253b665185
ujinthegrey <v4deus@gmail.com>
2024-02-26 11:23:32 +0400

    add loader for Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import AppLayout from './UI/AppLayout'
+import Error from './UI/Error'
+import Home from './UI/Home'
+import Menu, { loader as menuLoader } from './features/menu/Menu'
+import Cart from './features/cart/Cart'
+import CreateOrder from './features/order/CreateOrder'
+import Order from './features/order/Order'
+
+const router = createBrowserRouter([
+  { 
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/menu', element: <Menu/>, loader: menuLoader },
+      { path: '/cart', element: <Cart /> },
+      { path: '/order/new', element: <CreateOrder /> },
+      { path: '/order/:orderId', element: <Order /> },
+    ]
+  },
+])
+
 function App() {
-  //const nero = 616
-  return (
-    <div>Hello, Vite!</div>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
---
+++ src/UI/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+      <header>
+        <Link to="/" >Fast React Pizza</Link>
+        <p>Jonny Depp</p>
+      </header>
+    )
+  }
+  
+  export default Header
