c73d09fa899e8be37ce3a41bd3838a0d25bc35e8
ujinthegrey <v4deus@gmail.com>
2024-02-27 12:31:16 +0400

    add phobe validation and error message for it

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Form, redirect } from "react-router-dom"
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom"
 import { createOrder } from "../../services/apiRestaurant"
 
 // https://uibakery.io/regex-library/phone-number
@@ -7,8 +7,6 @@ const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
     str
   )
-console.log(isValidPhone)
-
 
 const fakeCart = [
   {
@@ -35,6 +33,11 @@ const fakeCart = [
 ]
 
 function CreateOrder() {
+  const navigation = useNavigation()
+  const isSubmitting = navigation.state === "submitting"
+
+  const formErrors = useActionData()
+
   const [withPriority, setWithPriority] = useState(false)
   const cart = fakeCart
 
@@ -54,6 +57,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -71,12 +75,12 @@ function CreateOrder() {
             value={withPriority}
             onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority">Want to give your order priority?</label>
         </div>
 
         <div>
           <input type='hidden' name="cart" value={JSON.stringify(cart)}/>
-          <button>Order now</button>
+          <button disabled={isSubmitting}>{isSubmitting ? 'Plasing order...' : 'Order now'}</button>
         </div>
       </Form>
     </div>
@@ -91,7 +95,14 @@ export async function action({ request }) {
     cart: JSON.parse(data.cart),
     priority: data.priority === "true"
   }
+
+  const errors = {}
+  if (!isValidPhone(order.phone)) errors.phone = 'Please enter you correct phone number. We might need co cinnect you.'
+  if (Object.keys(errors).length > 0) return errors
+
+  // if everithing is ok, create a new order and redirect
   const newOrder = await createOrder(order)
+
   return redirect(`/order/${newOrder.id}`)
 }
 
