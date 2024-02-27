cac0bb1baa06e3fe451fab3d56cab333973ce0b3
ujinthegrey <v4deus@gmail.com>
2024-02-27 18:28:50 +0400

    set up tailwind to the project

--- src/UI/Home.jsx
+++ src/UI/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
     return (
       <div>
-        <h1>
+        <h1 className="text-2xl text-red-400 font-bold">
           The best pizza.
           <br />
           Straight out of the oven, straight to you.
